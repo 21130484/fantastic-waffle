f0a961338e814d119ced277b6309fdebf2b20341
Toanphuoc2611 <toanphuoc26112003@gmail.com>
2024-05-29 14:28:15 +0700

    commit

--- Food/app/src/main/java/DAO/DatabaseHelper.java
+++ Food/app/src/main/java/DAO/DatabaseHelper.java
@@ -6,10 +6,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-
-import com.example.food.MainActivity;
-import com.example.food.R;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -21,33 +17,6 @@ import java.util.List;
 import model.Recipes;
 
 public class DatabaseHelper {
-
-    public static SQLiteDatabase initDatabase(Activity activity, String name) {
-        try {
-            String outFileName = activity.getApplicationInfo().dataDir +"/databases/" + name;
-            File f = new File(outFileName);
-            if (f.exists()) {
-                InputStream e = activity.getAssets().open(name);
-                File folder = new File(activity.getApplicationInfo().dataDir +"/databases/");
-                if (!folder.exists()) {
-                    folder.mkdirs();
-                }
-                FileOutputStream output = new FileOutputStream(outFileName);
-                byte[] buffer = new byte[1024];
-
-                int length;
-                while ((length = e.read(buffer)) > 0) {
-                    output.write(buffer, 0, length);
-                }
-                output.flush();
-                output.close();
-                e.close();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return activity.openOrCreateDatabase(name, Context.MODE_PRIVATE, null);
-    }
 
     public static List<Recipes> getAllRecipes(SQLiteDatabase db) {
         List<Recipes> recipesList = new ArrayList<>();
@@ -66,32 +35,6 @@ public class DatabaseHelper {
 
         cursor.close();
         return recipesList;
-    }
-    public static List<Recipes> getListSearch(SQLiteDatabase db, String textSearch) {
-            List<Recipes> result = new ArrayList<>();
-            Cursor cursor = db.rawQuery("SELECT * FROM recipes WHERE title LIKE ?", new String[]{"%" + textSearch + "%"});
-
-            if (cursor.moveToFirst()) {
-                do {
-                    int id = cursor.getInt(0);
-                    String title = cursor.getString(1);
-                    String instructions = cursor.getString(2);
-                    String image = cursor.getString(3);
-                    Recipes recipe = new Recipes(id, title, instructions, image);
-                    result.add(recipe);
-                } while (cursor.moveToNext());
-            }
-
-            cursor.close();
-            return result;
-        }
-
-        public static long addFavoriteRecipe(Activity activity, int userId, int recipeId) {
-        ContentValues values = new ContentValues();
-        values.put("user_id", userId);
-        values.put("recipe_id", recipeId);
-        SQLiteDatabase db = initDatabase(activity, "data.db");
-        return db.insert("favorite_recipes", null, values);
     }
 
     public static List<Recipes> getRecipeFavorite(SQLiteDatabase db, int user_id) {
